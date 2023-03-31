07a77f98700939887e51c8a8970bab41fd6e4e5a
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-31 10:13:32 +0000

    Implement Like model

---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name 'User'
+  belongs_to :post
+  after_create :update_likes_counter
+
+  def update_likes_counter
+    post.update(LikesCounter: Like.where(post_id).count)
+  end
+end
