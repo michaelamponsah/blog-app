92911f6dc1f71e5277ff856b12dc7d6df0a71247
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-31 10:23:44 +0000

    Implement Post model

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many: comments
+  has_many: likes
+  after_save: update_user_post_count
+
+  def update_user_post_count
+    author.update(posts_count: Post.where(author_id).count)
+  end
+
+  def get_recent_comments(limit_count = 5)
+    comments.order(created_at: :desc).limit(limit_count)
+  end
+end
