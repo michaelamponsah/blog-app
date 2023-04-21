c747df036d8f7d3e35427de0c52535121217f9f1
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-04-21 17:16:29 +0000

    Add tests for single user posts show

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
+
 RSpec.describe 'posts/index', type: :feature do
   before :each do
     @img = 'https://picsum.photos/id/870/200/300?grayscale&blur=2'
     @author = User.create(name: 'Dan', photo: @img, bio: 'Teacher from Mexico', posts_counter: 2)
+
     visit root_path
+
     @first_post = Post.create(author: @author, title: 'Dan Post #1', text: "This is Dan's first post")
     @second_post = Post.create(author: @author, title: 'Dan Post #2', text: "This is Dan's second post")
     @first_comment = Comment.create(post: @first_post, author: @author, text: 'First comment for Dan')
     @second_comment = Comment.create(post: @second_post, author: @author, text: 'Second comment for Dan')
     @first_like = Like.create(post: @first_post, author: @author)
     @second_like = Like.create(post: @second_post, author: @author)
+
     visit user_posts_path(@author)
   end
   it 'shows the user\'s profile picture' do
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe 'users/index', type: :feature do
 
     @first_user = User.first
 
-    @post1 = Post.create(author: @first_user, title: 'Hello', text: 'This is my second test post',
+    @post1 = Post.create(author: @first_user, title: 'Post It', text: 'Post 1',
                          comments_counter: 2, likes_counter: 6)
-    @post2 = Post.create(author: @first_user, title: 'My second post',
-                         text: 'Yeah, this is a new post for the first user', comments_counter: 0, likes_counter: 3)
-    @post3 = Post.create(author: @first_user, title: 'My third post', text: 'Added a new post', comments_counter: 4,
+    @post2 = Post.create(author: @first_user, title: 'Another random post',
+                         text: 'Some post', comments_counter: 0, likes_counter: 3)
+    @post3 = Post.create(author: @first_user, title: 'Post 3', text: 'A new post', comments_counter: 4,
                          likes_counter: 1)
 
     visit user_path(@first_user)
@@ -45,14 +45,14 @@ RSpec.describe 'users/index', type: :feature do
     expect(page).to have_content(@first_user.bio)
   end
 
-  it 'shows the users first three post' do
-    expect(page).to have_content(@post1.text)
-    expect(page).to have_content(@post2.text)
-    expect(page).to have_content(@post3.text)
+  it 'shows the users first three posts' do
+    expect(page).to have_content(@post1.title)
+    expect(page).to have_content(@post2.title)
+    expect(page).to have_content(@post3.title)
   end
 
   it 'shows a button to view all users posts' do
-    expect(page).to have_content('See All Posts')
+    expect(page).to have_content('See all posts')
   end
 
   it 'redirects to the posts show page when I click on a user' do
@@ -62,7 +62,7 @@ RSpec.describe 'users/index', type: :feature do
   end
 
   it 'redirects to the posts show page when I click on a user' do
-    click_link 'See All Posts'
+    click_link 'See all posts'
     expect(current_path).to match user_posts_path(@first_user)
   end
 end
