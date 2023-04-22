a89d279b9ef1ff7f1d7fca3c99bc50dc83b4def0
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-04-22 14:46:40 +0000

    Implement styles for login and sign-up pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Default URL options
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
